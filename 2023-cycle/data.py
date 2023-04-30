dae2d9182c523845853ca2742e58b5f8fb43867c
a <>
2023-04-30 19:30:14 -0400

    ugly matplotlib lol

--- 2023-cycle/data.py
+++ 2023-cycle/data.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import json
+import matplotlib
 import matplotlib.pyplot as plt
 
 with open("result/result.json", encoding="iso-8859-1") as f:
@@ -17,10 +18,10 @@ c = sorted(colleges.items(), key=lambda x: x[1], reverse=True)
 s = sorted(interested_subjects.items(), key=lambda x: x[1], reverse=True)
 
 with open("../data/t25.txt") as f:
-    t25 = set(i.strip() for i in f.read().split("\n"))
+    t25 = [i.strip() for i in f.read().split("\n")]
 
 with open("../data/t50.txt") as f:
-    t50 = set(i.strip() for i in f.read().split("\n"))
+    t50 = [i.strip() for i in f.read().split("\n")]
 
 t25_count = sum(colleges[i] for i in t25)
 t50_count = sum(colleges[i] for i in t50)
@@ -29,3 +30,32 @@ total = sum(colleges.values())
 
 print(f"People in T20(25): {t25_count}, % of commitments: {t25_count / total * 100 : .2f}%")
 print(f"People in T50: {t50_count}, % of commitments: {t50_count / total * 100 : .2f}%")
+
+# Matplotlib
+vals = [colleges[i] for i in t25]
+
+fig, ax = plt.subplots(figsize=(16, 9))
+
+ax.barh(t25, vals)
+
+for s in ['top', 'bottom', 'left', 'right']:
+    ax.spines[s].set_visible(False)
+
+ax.xaxis.set_tick_params(pad=5)
+ax.yaxis.set_tick_params(pad=10)
+
+ax.grid(color='grey',
+        linestyle='-.', linewidth=0.5,
+        alpha=0.2)
+
+ax.invert_yaxis()
+
+for i in ax.patches:
+    plt.text(i.get_width()+0.2, i.get_y()+0.5,
+             str(round((i.get_width()), 2)),
+             fontsize = 10, fontweight ='bold',
+             color ='grey')
+
+ax.set_title("University vs Commitments")
+
+plt.show()
